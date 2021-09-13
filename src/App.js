4c04e17acd281b7004817959685f05b6baa4a2e0
unknown <devanaru2001@gmail.com>
2021-09-13 15:15:54 +0530

    small

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div >
        <Navigation />
-         <Route path='/' exact>
+         <Route path='/acme' exact>
             <Home /> 
          </Route>
          <Route path='/About' >
